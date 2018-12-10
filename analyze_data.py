115b65252c36c6f708f222a27cff677c64124402
Valentin Dmitrochenko <val2828@tut.by>
2018-12-10 17:52:15 +0100

    added new timestamp datetime formatting

--- analyze_data.py
+++ analyze_data.py
@@ -1,3 +1,11 @@
-import data_reader
+import data_reader as dr
+# specify run parameters
+start_date = "2018/11/29 00:00:00"
+end_date = "2018/12/01 23:59:59"
 
-data_reader
+# add information to the data
+
+# run
+
+dr.preprocess()
+filtered_data = dr.filter_per_date(start_date,end_date)
--- data_reader.py
+++ data_reader.py
@@ -1,18 +1,48 @@
 import numpy as np
 import pandas as pa
+from datetime import datetime as dt
+# Change the date format
 
-# read the csv
+
+#read the csv
 path = "log.csv"
 data = pa.read_csv(path)
-# change Case IDs
-case_ids = data['Case ID']
-extracted_ids = {}
-new_case_ids = []
-counter = 0
-for entry in case_ids:
-    if entry not in extracted_ids.keys():
-        extracted_ids[entry] = counter
-        counter += 1
-    new_case_ids.append(extracted_ids[entry])
-data['Case ID'] = new_case_ids
-print(data['Case ID'])
+deployment_history = pa.read_csv('deployment_history.csv')
+
+# change the timestamps into datetime objects
+deployment_history = pa.to_datetime(deployment_history['Deployed on'], format='%d-%m-%y %H:%M',errors='ignore')
+data['Start Timestamp'] = pa.to_datetime(data['Start Timestamp'], format='%Y/%m/%d %H:%M',errors='ignore')
+data['Complete Timestamp'] = pa.to_datetime(data['Complete Timestamp'], format='%Y/%m/%d %H:%M',errors='ignore')
+
+# change Case IDs, add deployemnts and optionally create a csv output to the specialized folder
+def preprocess(print_to_file=False):
+    # assign new ids
+    case_ids = data['Case ID']
+    extracted_ids = {}
+    new_case_ids = []
+    counter = 0
+    for entry in case_ids:
+        if entry not in extracted_ids.keys():
+            extracted_ids[entry] = counter
+            counter += 1
+        new_case_ids.append(extracted_ids[entry])
+    data['Case ID'] = new_case_ids
+
+    # add deployments
+    # for timestamp in data['Complete Timestamp']:
+    #     for dep_date in deployment_history['Deployed on']:
+    #         if (timestamp < dep_date):
+    #             data['Deployment'] = deployment_history['Model']
+
+    #print(data[['Complete Timestamp', 'Deployment']])
+# filter per date
+def filter_per_date(start_date, end_date):
+    filtered_data = data[(data['Start Timestamp'] > start_date) & (data['Complete Timestamp'] < end_date)]
+    return filtered_data
+
+
+# extract the resources
+
+
+
+# assign de
