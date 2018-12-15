3c7d9617ba12258bcda4989a717fcb0b388640d5
Valentin Dmitrochenko <val2828@tut.by>
2018-12-15 12:32:11 +0100

    Working filter, deployements assignment. Started with deploymennt stats

--- analyze_data.py
+++ analyze_data.py
@@ -2,11 +2,12 @@ import data_reader as dr
 import datetime as dt
 import numpy as np
 # specify run parameters
-start_date = dt.datetime.strptime("2018/12/01 00:00:00", '%Y/%m/%d %H:%M:%S')
-end_date = dt.datetime.strptime("2018/12/11 23:59:59", '%Y/%m/%d %H:%M:%S')
+start_date = dt.datetime.strptime("2018/12/13 00:00:00", '%Y/%m/%d %H:%M:%S')
+end_date = dt.datetime.strptime("2018/12/13 23:59:59", '%Y/%m/%d %H:%M:%S')
 
 def get_stats_per_deployment(data):
     stats_summary_per_deployment = {}
+
 
 
 
@@ -22,7 +23,7 @@ def get_stats_per_deployment(data):
 # run
 
 dr.preprocess()
-filtered_data = dr.filter_per_date(start_date,end_date, False)
+filtered_data = dr.filter_per_date(start_date,end_date, True)
 
 get_stats_per_deployment(filtered_data)
 
